bd558910ce8f4f5fd8ebaf40c80fc7a23649014e
Oiertron <oiermarcos79@gmail.com>
2024-07-05 11:00:43 +0200

    first commit

---
+++ app/src/main/java/com/example/reto/ui/courses/CoursesFragment.java
@@ -0,0 +1,61 @@
+package com.example.reto.ui.courses;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.reto.adapter.CoursesAdapter;
+import com.example.reto.data.Courses;
+import com.example.reto.databinding.FragmentCoursesBinding;
+
+import java.util.ArrayList;
+
+public class CoursesFragment extends Fragment {
+
+    private FragmentCoursesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CoursesViewModel homeViewModel =
+                new ViewModelProvider(this).get(CoursesViewModel.class);
+
+        binding = FragmentCoursesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        ListView coursesListView = binding.lvAlmiCourse;
+
+
+        ArrayList<Courses> coursesArrayList = new ArrayList<>();
+        coursesArrayList.add(new Courses("Day", "SMR1", 990));
+        coursesArrayList.add(new Courses("Day", "SMR2", 20));
+        coursesArrayList.add(new Courses("Day", "ASIR1", 30));
+        coursesArrayList.add(new Courses("Day", "ASIR2", 10));
+        coursesArrayList.add(new Courses("Day", "DAM1", 20));
+        coursesArrayList.add(new Courses("Day", "DAM2", 30));
+        coursesArrayList.add(new Courses("Day", "AF1", 10));
+        coursesArrayList.add(new Courses("Day", "AF2", 20));
+        coursesArrayList.add(new Courses("Day", "FPB1", 30));
+        coursesArrayList.add(new Courses("Day", "FPB2", 10));
+        coursesArrayList.add(new Courses("Evening", "Cae1", 20));
+        coursesArrayList.add(new Courses("Evening", "Cae2", 30));
+
+
+        CoursesAdapter adapter = new CoursesAdapter(getContext(), coursesArrayList);
+        coursesListView.setAdapter(adapter);
+
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
